9ab2c9811e7c454ff351c9859f12b8bc9f6356da
pkepp11-7 <pkpitcher@hotmail.com>
2019-04-21 21:45:00 -0700

    Add version 1 of align (step 3c)

--- Project3-KepplerWright/src/ReadMapApp.cpp
+++ Project3-KepplerWright/src/ReadMapApp.cpp
@@ -48,9 +48,63 @@ void ReadMapApp::findLoc()
 
 }
 //Step 3c: align
-void ReadMapApp::align()
+void ReadMapApp::align(const vector<unsigned int> & indexList, Sequence * G, Sequence * r_i)
 {
+    unsigned int startPos, endPos, l, bestCoverage, j0, j1;
+    int length = indexList.size();
+    Alignment readAlignment;
+    AlignmentData aData;
+    Sequence G_substring = {G->name, ""};
 
+    bestCoverage = 0;
+
+    //for each potential "best hit"
+    for(const unsigned int & j : indexList) 
+    {
+        //get the length of the read
+        l = r_i->nucleotideSequence.length();
+
+        //check for edge cases for values of j
+        if(j < l)
+        {
+            //if j - l < 0, set to start at 0
+            startPos = 0;
+        }
+        else
+        {
+            //else start at position j - l
+            startPos = j - l;
+        }
+        if(j + l > length)
+        {
+            //if j + l exceeds the end of G, end at the end of G
+            endPos = length;
+        }
+        else
+        {
+            //else end at j + l
+            endPos = j + l;
+        }
+        //Create a Sequence with a substring of G, based on previous startPos and endPos
+        G_substring.nucleotideSequence = G->nucleotideSequence.substr(startPos, endPos - startPos);
+
+        //perform alignment on the two strings
+        readAlignment = Alignment(*r_i, G_substring);
+        readAlignment.doLocalAlignment();
+        aData = readAlignment.getData();
+
+        //if the percent identity is > 90% and the length coverage > 80%
+        if(aData.percentIdentity > 90 && aData.lengthCoverage > 80) 
+        {
+            if(aData.lengthCoverage > bestCoverage)
+            {
+                //if this is the best coverage encountered for any j in Li, save it
+                j0 = aData.bestHit_j0 + startPos;
+                j1 = aData.betsHit_j1 + startPos;
+                bestCoverage = aData.lengthCoverage;
+            }
+        }
+    }
 }
 //Step 4: output
 void ReadMapApp::output()
